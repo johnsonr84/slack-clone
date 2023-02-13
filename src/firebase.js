d6ee7dbabc104d5b143bafe769823693a08c6426
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-12 17:48:19 -0700

    add firebase.js for config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgEokB6Wd2v9Ph0e9aILq_Oms_JjjjjJM",
+    authDomain: "slack-clone-dd-71ab5.firebaseapp.com",
+    projectId: "slack-clone-dd-71ab5",
+    storageBucket: "slack-clone-dd-71ab5.appspot.com",
+    messagingSenderId: "632560500032",
+    appId: "1:632560500032:web:e2dd1bd223751abac78ea5"
+  };
