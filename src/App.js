87fa1b9a10f887343d1e34a7ad5cc5add487c017
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-13 23:50:58 -0700

    create header component, add icons and styled divs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
+import Header from "./components/Header";
+// import styled from "styled-components";
+
 function App() {
   return (
   
@@ -9,7 +12,8 @@ function App() {
         <>
         <Switch>
           <Route path="/">
-            <h1>This is the homepage</h1>
+          <Header />
+
           </Route>
         </Switch>
         </>
