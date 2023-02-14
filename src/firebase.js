e51d5b97b8cb98d1c714ef8e29dfa02ac5325305
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-14 00:28:04 -0700

    update firebase with db, auth and provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDgEokB6Wd2v9Ph0e9aILq_Oms_JjjjjJM",
     authDomain: "slack-clone-dd-71ab5.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "632560500032",
     appId: "1:632560500032:web:e2dd1bd223751abac78ea5"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
